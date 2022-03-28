73f5d7ccf874588531414ff2e19d247a775458dd
Mikalai Stelmakh <mikalai.stelmakh.stud@pw.edu.pl>
2022-03-28 16:27:58 +0200

    Remove numpy import

--- lab2/zadanie_domowe/pwm.py
+++ lab2/zadanie_domowe/pwm.py
@@ -1,8 +1,6 @@
 from typing import Dict, List
+import argparse
 import math
-import time
-import numpy as np
-import matplotlib.pyplot as plt
 
 
 TIME_INTERVAL = 1_000
@@ -14,7 +12,7 @@ def generate_pwm(frequencies: List[int], fillings: List[int], seconds: int = Non
     if len(frequencies) != 1 and len(fillings) == 1:
         fillings = [fillings[0] for _ in frequencies]
 
-    t = np.arange(0, seconds, 1/TIME_INTERVAL)
+    t = [n/TIME_INTERVAL for n in range(seconds * TIME_INTERVAL)]
     t_each_frequency = seconds/len(frequencies)
     current_index = 0
     pwm = []
@@ -22,7 +20,7 @@ def generate_pwm(frequencies: List[int], fillings: List[int], seconds: int = Non
     for frequency, filling in zip(frequencies, fillings):
         period = 1 / frequency
         next_index = current_index + math.ceil(t_each_frequency*TIME_INTERVAL)
-        pwm.extend(t[current_index:next_index] % period < period * filling)
+        pwm.extend([time % period < period * filling for time in t[current_index:next_index]])
         current_index = next_index
 
     result = {time_mark: state for time_mark, state in zip(t, pwm)}
@@ -30,7 +28,15 @@ def generate_pwm(frequencies: List[int], fillings: List[int], seconds: int = Non
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--draw", action="store_true",
+                        help="show the plot")
+    args = parser.parse_args()
+
     t_pwm = generate_pwm([4, 2], [0.3], 4)
     t, pwm = t_pwm.keys(), t_pwm.values()
-    plt.plot(t, pwm)
-    plt.show()
+
+    if args.draw:
+        import matplotlib.pyplot as plt
+        plt.plot(t, pwm)
+        plt.show()
