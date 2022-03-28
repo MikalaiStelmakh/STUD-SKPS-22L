81f54736b434d897857616d684a7ab1a5a309994
Mikalai Stelmakh <mikalai.stelmakh.stud@pw.edu.pl>
2022-03-28 16:28:35 +0200

    Add argparser

--- lab2/zadanie_domowe/pwm_variable_filling.py
+++ lab2/zadanie_domowe/pwm_variable_filling.py
@@ -1,10 +1,21 @@
+import argparse
 from pwm import generate_pwm
-import matplotlib.pyplot as plt
 
 
-time = 5
-t_pwm = generate_pwm([3], [.3, .5, .7, .1, .9], time)
-t, pwm = t_pwm.keys(), t_pwm.values()
-plt.plot(t, pwm)
-plt.xticks([n for n in range(time+1)])
-plt.savefig("img/variable_filling")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-frequency", type=int, default=1,
+                        help="frequency (Hz)")
+    parser.add_argument("-seconds", type=int, default=5,
+                        help="timeout (s)")
+    parser.add_argument("--draw", action="store_true",
+                        help="show the plot")
+    args = parser.parse_args()
+
+    t_pwm = generate_pwm([args.frequency], [.3, .5, .7, .1, .9], args.seconds)
+    if args.draw:
+        import matplotlib.pyplot as plt
+        t, pwm = t_pwm.keys(), t_pwm.values()
+        plt.plot(t, pwm)
+        plt.xticks([n for n in range(args.seconds+1)])
+        plt.show()
--- lab2/zadanie_domowe/pwm_variable_frequency.py
+++ lab2/zadanie_domowe/pwm_variable_frequency.py
@@ -1,10 +1,21 @@
+import argparse
 from pwm import generate_pwm
-import matplotlib.pyplot as plt
 
 
-time = 6
-t_pwm = generate_pwm([5, 2, 4, 1, 3, 1], [.3], time)
-t, pwm = t_pwm.keys(), t_pwm.values()
-plt.plot(t, pwm)
-plt.xticks([n for n in range(time+1)])
-plt.savefig("img/variable_frequency")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-filling", type=float, default=1,
+                        help="filling (from 0 to 1)")
+    parser.add_argument("-seconds", type=int, default=5,
+                        help="timeout (s)")
+    parser.add_argument("--draw", action="store_true",
+                        help="show the plot")
+    args = parser.parse_args()
+
+    t_pwm = generate_pwm([5, 2, 4, 1, 3, 1], [args.filling], args.seconds)
+    if args.draw:
+        import matplotlib.pyplot as plt
+        t, pwm = t_pwm.keys(), t_pwm.values()
+        plt.plot(t, pwm)
+        plt.xticks([n for n in range(args.seconds+1)])
+        plt.show()
