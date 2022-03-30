426e40f5d6bbd6f60151389714818ebacdf8d286
Eryk Sztanga <01150115@pw.edu.pl>
2022-03-30 12:20:27 +0200

    Zadanie 2

--- lab2/pwm.py
+++ lab2/pwm.py
@@ -1,8 +1,6 @@
 from typing import Dict, List
-import math
 import time
-import numpy as np
-import matplotlib.pyplot as plt
+import math
 
 
 TIME_INTERVAL = 1_000
@@ -14,7 +12,7 @@ def generate_pwm(frequencies: List[int], fillings: List[int], seconds: int = Non
     if len(frequencies) != 1 and len(fillings) == 1:
         fillings = [fillings[0] for _ in frequencies]
 
-    t = np.arange(0, seconds, 1/TIME_INTERVAL)
+    t = [n/TIME_INTERVAL for n in range(seconds * TIME_INTERVAL)]
     t_each_frequency = seconds/len(frequencies)
     current_index = 0
     pwm = []
@@ -22,27 +20,23 @@ def generate_pwm(frequencies: List[int], fillings: List[int], seconds: int = Non
     for frequency, filling in zip(frequencies, fillings):
         period = 1 / frequency
         next_index = current_index + math.ceil(t_each_frequency*TIME_INTERVAL)
-        pwm.extend(t[current_index:next_index] % period < period * filling)
+        pwm.extend([time % period < period * filling for time in t[current_index:next_index]])
         current_index = next_index
 
     result = {time_mark: state for time_mark, state in zip(t, pwm)}
     return result
 
-
-def control_state(t_pwm):
+def control_state(t_pwm, controller):
     prev_state = True
     prev_t = 0
     for t, state in zip(t_pwm.keys(), t_pwm.values()):
         time_sleep = t - prev_t
         if state != prev_state:
-            #controller.write(state)
+            controller.value = state
             prev_state = state 
         time.sleep(time_sleep)
         prev_t = t
 
 
 if __name__ == "__main__":
-    t_pwm = generate_pwm([4, 2], [0.3], 4)
-    t, pwm = t_pwm.keys(), t_pwm.values()
-    plt.plot(t, pwm)
-    plt.show()
+    
---
+++ lab2/zad2.py
@@ -0,0 +1,19 @@
+import gpio4
+import time
+import sys
+from pwm import generate_pwm, control_state
+
+class GPIOwrap(gpio4.SysfsGPIO):
+    def __init__(self, pin, direction):
+        super().__init__(pin)
+        self.export = True
+        self.direction = direction
+
+    def __del__(self):
+        self.export = False
+
+pin = 23
+if __name__ == "__main__": 
+    pin23 = GPIOwrap(pin, "out")
+    t_pwm = generate_pwm([2], [0.3, 0.5, 0.1, 0.5], 10)
+    control_state(t_pwm, pin23)
