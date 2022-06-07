7ba11bc99e81ce63a248142cc0c203e231b5d7d4
Mikalai Stelmakh <mikalai.stelmakh@gmail.com>
2022-06-07 18:21:31 +0200

    Add gui

--- project/client.py
+++ project/client.py
@@ -1,4 +1,8 @@
 import socket
+import matplotlib.pyplot as plt
+import numpy as np
+from math import radians
+
 
 ADDRESS = ''
 PORT = 8080
@@ -8,12 +12,31 @@ def receive_measurement():
     data = str(soc.recv(1024))[2:-1].split(',')
     angle, distance = data
     print(angle, distance)
+    return int(angle), int(distance)
 
 
-soc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-soc.connect((ADDRESS, PORT))
+def plotRadial():
+    fig = plt.figure()
+    fig.add_subplot(111, projection='polar' )
+    plt.draw()
+    plt.pause(0.00000001)
+    return fig
 
-soc.send(b"")
 
-while True:
-    receive_measurement()
+def addPoint(angle, distance):
+    plt.scatter([radians(angle)], [distance], marker='o', s=1, alpha=1, c='red')
+    plt.draw()
+    plt.pause(0.00000001)
+
+
+if __name__ == "__main__":
+    soc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    soc.connect((ADDRESS, PORT))
+
+    soc.send(b"")
+
+    fig = plotRadial()
+
+    while plt.fignum_exists(fig.number):
+        angle, distance = receive_measurement()
+        addPoint(angle, distance)
